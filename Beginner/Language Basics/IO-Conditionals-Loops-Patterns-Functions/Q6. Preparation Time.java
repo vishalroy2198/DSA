69dfb462bc57f3880090c2ff6c9228e3c653b083
Vishal Kr. Roy <78053101+vishalroy2198@users.noreply.github.com>
2022-08-31 00:29:38 +0530

    Create Q6. Preparation Time.java

---
+++ Beginner/Language Basics/IO-Conditionals-Loops-Patterns-Functions/Q6. Preparation Time.java
@@ -0,0 +1,21 @@
+/*You're going to write some code to help you cook a gorgeous lasagna from your favorite cookbook.
+Now, you want to also add few layers to the lasagna. Assume each layer takes 2 minutes to prepare.
+
+Given the number of layers you want to add to the lasagna, find how many minutes you would spend making them.*/
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(2*n);
+        
+    }
+}
