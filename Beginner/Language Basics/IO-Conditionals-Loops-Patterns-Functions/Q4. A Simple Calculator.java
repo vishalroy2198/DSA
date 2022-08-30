b1f55f4bcd76bc991d7ac2199c766388fd216e69
Vishal Kr. Roy <78053101+vishalroy2198@users.noreply.github.com>
2022-08-31 00:25:48 +0530

    Create Q4. A Simple Calculator.java

---
+++ Beginner/Language Basics/IO-Conditionals-Loops-Patterns-Functions/Q4. A Simple Calculator.java
@@ -0,0 +1,26 @@
+/*Given two numbers A and B. Print A+B, A*B, A-B, A/B in the same order.*/
+
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(a+b);
+        System.out.print(" ");
+        System.out.print(a*b);
+        System.out.print(" ");
+        System.out.print(a-b);
+        System.out.print(" ");
+        System.out.print(a/b);
+        
+        
+    }
+}
