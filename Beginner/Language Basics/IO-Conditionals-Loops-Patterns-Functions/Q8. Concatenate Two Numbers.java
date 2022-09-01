da4838697c49109c4d67db53665815f120c77f82
Vishal Kr. Roy <78053101+vishalroy2198@users.noreply.github.com>
2022-09-01 21:16:33 +0530

    Create Q8. Concatenate Two Numbers.java

---
+++ Beginner/Language Basics/IO-Conditionals-Loops-Patterns-Functions/Q8. Concatenate Two Numbers.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner sc = new Scanner(System.in);
+
+        int a,b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.print(a);
+        System.out.print(b);
+        
+    }
+}
