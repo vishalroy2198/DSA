627098b4e5f14dff3fdfe8b4c231f2fc4132a65b
Vishal Kr. Roy <78053101+vishalroy2198@users.noreply.github.com>
2022-09-01 21:17:43 +0530

    Create Q39 Add 10.java

---
+++ Beginner/Language Basics/IO-Conditionals-Loops-Patterns-Functions/Q39 Add 10.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+
+        Scanner sc = new Scanner(System.in);
+
+        int f = sc.nextInt();
+
+        System.out.print(10 + f);
+        
+    }
+}
